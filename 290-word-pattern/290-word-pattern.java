435987b2e29fec53074949fb47b143981f1d0724
Nivethitha21 <59059126+Nivethitha21@users.noreply.github.com>
2022-01-17 21:35:14 +0530

    Time: 2 ms (30.18%), Space: 38.1 MB (41.98%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words= str.split(" ");
+        HashMap<Character, String> map = new HashMap<Character, String>();
+        if(words.length!= pattern.length())
+            return false;
+        for(int i=0; i<words.length; i++){
+            char c = pattern.charAt(i);
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(words[i]))
+                    return false;
+            }else{
+                if(map.containsValue(words[i]))
+                    return false;
+                map.put(c, words[i]);
+            }    
+        }
+        return true;
+    }
+}
